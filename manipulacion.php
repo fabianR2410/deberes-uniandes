f11ac97fb95a6951637882b80dcc20ee75215bd9
fabianR2410 <fabianrc24@uniandes.edu.ec>
2024-07-31 15:49:12 -0500

    actualizacion

--- manipulacion.php
+++ manipulacion.php
@@ -7,4 +7,9 @@ $cadena2 = "mundo";
 //concatnar
 $cadena_concatenada = $cadena1 . $cadena2;
 echo $cadena_concatenada; //print cadena concatenada
+
+
+// longitud de la cadena
+$longitud = strlen($cadena_concatenada);
+echo "<br>La longitud de la cadena es: " . $longitud;
 ?>
