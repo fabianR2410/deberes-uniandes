4aab7a7405975591b1a20a5526120242a0807be5
fabianR2410 <fabianrc24@uniandes.edu.ec>
2024-07-31 15:44:54 -0500

    actualizacion

---
+++ manipulacion.php
@@ -0,0 +1,10 @@
+<?php
+//variables
+
+$cadena1 = "hola<br>";
+$cadena2 = "mundo";
+
+//concatnar
+$cadena_concatenada = $cadena1 . $cadena2;
+echo $cadena_concatenada; //print cadena concatenada
+?>
