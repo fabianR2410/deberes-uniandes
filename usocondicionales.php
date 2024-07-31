41fd2c6d9029ba1abac1b0d216582da2d857a17e
fabianR2410 <fabianrc24@uniandes.edu.ec>
2024-07-31 15:53:19 -0500

    actualizacion

---
+++ usocondicionales.php
@@ -0,0 +1,11 @@
+<?php
+// Definir la variable booleana
+$variable_booleana = false;
+
+// Estructura de control condicional
+if ($variable_booleana) {
+    echo "La variable booleana es verdadera.";
+} else {
+    echo "La variable booleana es falsa.";
+}
+?>
