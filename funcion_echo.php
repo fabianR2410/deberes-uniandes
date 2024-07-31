bc921238079d61793237f035bcd7d01a4bf97872
fabianR2410 <fabianrc24@uniandes.edu.ec>
2024-07-31 15:37:35 -0500

    actualizacion

---
+++ funcion_echo.php
@@ -0,0 +1,15 @@
+<?php
+$int = 10;
+$float = 10.2;
+$boleano = true;
+$numeros= array("1<br>","2<br>","3<br>");
+
+echo $int;
+echo "<br>";
+echo $float;
+echo "<br>";
+echo $boleano;
+echo "<br>";
+echo $numeros[2];
+
+?>
